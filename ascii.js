adf41f034221ca0bb2a7163cfc683c7fbb43b99d
byambab <bumnaranb@gmail.com>
2020-03-09 20:02:40 -0500

    Create ascii.js

---
+++ ascii.js
@@ -0,0 +1,77 @@
+
+var timeout;
+var currentFrame;
+var animationType;
+var animations;
+
+function onClickStart() {
+  $('#btnStart').attr('disabled', true);
+  $('#btnStop').attr('disabled', false);
+
+  currentFrame = 0;
+  if (!animationType) {
+    animationType = $('#combo-animation option:selected').val();
+  }
+
+  // Access ANIMATIONS
+  if (!animations) {
+    animations = ANIMATIONS[animationType].split('=====\n');
+  }
+
+  const textArea = $('#mytextarea');
+  timeout = setTimeout(function() { doAnimation(textArea); }, calculateInterval());
+}
+
+function onClickStop() {
+  $('#btnStart').attr('disabled', false);
+  $('#btnStop').attr('disabled', true);
+  if (timeout) {
+    clearTimeout(timeout);
+    timeout = null;
+  }
+}
+
+function calculateInterval() {
+  const isTurbo = $('#cbox-turbo').is(':checked');
+  let interval = 250;
+  if (isTurbo) {
+    interval = 50;
+  }
+  return interval;
+}
+
+//////////////////////////////////////////
+
+function doAnimation(textArea) {
+  const len = animations.length;
+  if (len > 0) {
+    const displayContent = animations[currentFrame];
+    if (displayContent) {
+      textArea.val(displayContent);
+      currentFrame++;
+      if (currentFrame >= len) {
+        currentFrame = 0;
+      }
+    }
+  }
+  timeout = setTimeout(function() { doAnimation(textArea); }, calculateInterval());
+}
+
+function onChangeAnimation(element) {
+  animationType = element.value;
+  const value = ANIMATIONS[animationType];
+  if (value) {
+    const array = value.split('=====\n');
+    if (array && array.length > 0) {
+      currentFrame = 0;
+      animations = array;
+    }
+  }
+}
+
+function onChangeFontSize(element) {
+  $('#mytextarea').css('font-size', element.value);
+}
+
+
+
